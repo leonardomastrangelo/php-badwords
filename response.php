4d9f16a7d76cfe5f1e592bc2ed55b5ca3d76c908
leonardomastrangelo <leo.mastrangelo.st@gmail.com>
2023-11-27 15:40:08 +0100

    completed

--- response.php
+++ response.php
@@ -1,7 +1,12 @@
 <?php
+// text & length
 $paragraph = $_GET["paragraph"];
 $paragraphLength = strlen($paragraph);
+// censure & text & length
 $censure = $_GET["censure"];
+$censuredText = str_replace($censure, "***", $paragraph);
+$censuredTextLength = strlen($censuredText);
+
 ?>
 
 <!DOCTYPE html>
@@ -10,6 +15,8 @@ $censure = $_GET["censure"];
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
     <title>PHP Badwords Response</title>
 </head>
@@ -18,6 +25,7 @@ $censure = $_GET["censure"];
     <header>
         <h1>PHP Badwords Response</h1>
         <main class="container">
+            <!-- text & length -->
             <div>
                 <h2>
                     Il tuo testo:
@@ -28,6 +36,20 @@ $censure = $_GET["censure"];
                 <span>
                     Lunghezza del testo:
                     <?php echo $paragraphLength ?>
+                </span>
+            </div>
+
+            <!-- censure & text & length -->
+            <div>
+                <h2>
+                    Testo con censura:
+                </h2>
+                <p>
+                    <?php echo $censuredText ?>
+                </p>
+                <span>
+                    Lunghezza del testo censurato:
+                    <?php echo $censuredTextLength ?>
                 </span>
             </div>
         </main>
